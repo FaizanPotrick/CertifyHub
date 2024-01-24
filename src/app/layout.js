23994b4a1fe231d6319e3de87b7d318ed791b9c2
FaizanPotrick <faizan.potrick02@gmail.com>
2024-01-24 16:27:00 +0530

    Change folder structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
 import ReduxProvider from "@/redux/provider";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
